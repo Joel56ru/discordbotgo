d88340fe0122805ca1e6fef6b4eeeca3da884091
joel <volzhia@gmail.com>
2022-04-19 21:04:25 +0300

    корректировка

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
@@ -13,34 +16,54 @@ import (
 	"time"
 )
 
+type Topic struct {
+	Id         int    `json:"id"`
+	TopicTitle string `json:"topic_title"`
+	Body       string `json:"body"`
+	Forum      Forum  `json:"forum"`
+	CreatedAt  string `json:"created_at"`
+	Linked     Linked `json:"linked"`
+}
+type Forum struct {
+	Id        int    `json:"id"`
+	Position  int    `json:"position"`
+	Name      string `json:"name"`
+	Permalink string `json:"permalink"`
+	Url       string `json:"url"`
+}
+type Linked struct {
+	Id    int   `json:"id"`
+	Image Image `json:"image"`
+}
+type Image struct {
+	Original string `json:"original"`
+	Preview  string `json:"preview"`
+}
+
 var (
 	Token       string
-	TokenShiki  string
 	ChannelNews string
 	LastID      int
 )
 
 func main() {
-	ChannelNews = os.Getenv("CHANNELNEWS")
 	if err := godotenv.Load(); err != nil {
 		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
+	ChannelNews = os.Getenv("CHANNELNEWS")
 	Token = os.Getenv("DGU_TOKEN")
-	TokenShiki = os.Getenv("SHIKI_ACCESS_TOKEN")
+	LastID, _ = strconv.Atoi(os.Getenv("LASTID"))
 
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
-	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
-	ticker := time.NewTicker(time.Minute * 10)
+	ticker := time.NewTicker(time.Minute)
 
 	go func() {
 		for {
@@ -52,39 +75,32 @@ func main() {
 			}
 		}
 	}()
-	// Open a websocket connection to Discord and begin listening.
+
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	dg.Close()
-}
-func messageDELETE(s *discordgo.Session, m *discordgo.MessageDelete) {
-
-	s.ChannelMessageSend(m.ChannelID, "Удалил сообщение Ник: **fdf**")
-	log.Println(m.BeforeDelete.Author.Username)
-
 }
 
 func sendNews(s *discordgo.Session) {
 	var res []Topic
-	err := ShikiGetTopics(TokenShiki, &res)
+	err := ShikiGetTopics(&res)
 	if err != nil {
-		logrus.Fatal(err)
+		log.Fatal(err)
 	}
 	if LastID == res[0].Id {
 		return
 	}
 	LastID = res[0].Id
+	os.Setenv("LASTID", strconv.Itoa(res[0].Id))
 	embed := discordgo.MessageEmbed{
 		URL:         `https://shikimori.one` + res[0].Forum.Url + "/" + strconv.Itoa(res[0].Id),
 		Type:        "rich",
@@ -109,27 +125,42 @@ func sendNews(s *discordgo.Session) {
 	s.ChannelMessageSendEmbed(ChannelNews, &embed)
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
+	if m.Content == "жив?" {
 		s.ChannelTyping(m.ChannelID)
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
+		s.ChannelMessageSend(m.ChannelID, "жив!")
 	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
-
 	if m.Content == "новости тут будут" {
 		ChannelNews = m.ChannelID
+		os.Setenv("CHANNELNEWS", m.ChannelID)
 	}
+}
+
+func ShikiGetTopics(target interface{}) error {
+	spaceClient := &http.Client{
+		Timeout: time.Second * 2,
+	}
+	req, err := http.NewRequest(http.MethodGet, `https://shikimori.one/api/topics?limit=1&forum=news&page=1`, nil)
+	if err != nil {
+		return err
+	}
+	res, err := spaceClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &target)
+	if err != nil {
+		return err
+	}
+	return nil
 }
